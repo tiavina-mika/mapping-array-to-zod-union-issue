7fc70706db4c047ee78d24d56e216ce6424ce058
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-01-22 09:26:23 +0300

    fix issue

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const value0: OptionNotWorking = "xxxx"; // should trigger an error
 console.log("not working value", value0);
 
 // ----------------------------------------- //
-// ---------- this is not working ---------- //
+// -------------- this works --------------- //
 // ----------------------------------------- //
 export const optionsSchema = createManyUnion(["page", "blog"] as const);
 type Option = z.infer<typeof optionsSchema>;
@@ -49,10 +49,35 @@ const value: Option = "xxxx"; // trigger an error, so it working
 
 console.log("working value", value);
 
+const options2 = [
+  {
+    label: "page",
+    value: "page"
+  },
+  {
+    label: "Blog",
+    value: "blog"
+  }
+] as const;
+
+// ----------------------------------------- //
+// ---------------- solution --------------- //
+// ----------------------------------------- //
+const array2 = options2.map((option) => option.value);
+export const optionsWorkingSchema = createManyUnion(
+  array2 as typeof array2 & [string, string, ...string[]]
+);
+
+type OptionWorking = z.infer<typeof optionsWorkingSchema>;
+
+const value2: OptionWorking = "xxx"; // should trigger an error
+console.log("not working value", value2);
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Mapping readonly array to Zod union</h1>
+      <h1>Issue with mapping readonly array to Zod union</h1>
+      <code>(Fixed)</code>
     </div>
   );
 };
